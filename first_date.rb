a693e82b98e18d783c20f7b107e451d45c440529
James Davis <jamesmdavis5@gmail.com>
2013-09-25 10:16:59 -0400

    added comments to the file and notes for myself

--- first_date.rb
+++ first_date.rb
@@ -1,17 +1,27 @@
+# Ask the user how old they are
 puts "How old are you?"
 age = gets.chomp!
 
+#string interpolation
+puts "Cool I am #{age.to_i} too!"
+#string concentration
+puts "Cool I am " + age + " too!"
+
+# Ask if the user ever plans on having children
 puts "Do you ever plan on having children?"
 kids = gets.chomp!.downcase
 
+# If the user says yes to children ask for the childrens names
 if kids == "yes"
   puts "What will you name your children?"
   names = gets.chomp!
 
+# If the user is over 30 ask if they're married
   if age.to_i > 30
     puts "Are you married?"
     married = gets.chomp!.downcase
 
+# If the user is not married, ask if they ever plan on getting married
     if married == "no"
       puts "Do you ever plan on getting married?"
       get_married = gets.chomp!.downcase
