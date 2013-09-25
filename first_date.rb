6335bd555612130d51e06954ca9eaf3c5a58a71f
James Davis <jamesmdavis5@gmail.com>
2013-09-25 09:59:39 -0400

    reindented file

--- first_date.rb
+++ first_date.rb
@@ -8,17 +8,17 @@ if kids == "yes"
   puts "What will you name your children?"
   names = gets.chomp!
 
-if age.to_i > 30
-  puts "Are you married?"
-  married = gets.chomp!.downcase
+  if age.to_i > 30
+    puts "Are you married?"
+    married = gets.chomp!.downcase
 
-  if married == "no"
-    puts "Do you ever plan on getting married?"
-    get_married = gets.chomp!.downcase
+    if married == "no"
+      puts "Do you ever plan on getting married?"
+      get_married = gets.chomp!.downcase
 
-  if get_married == "yes"
-    puts "That is great I hope things work out with us!!"
+      if get_married == "yes"
+        puts "That is great I hope things work out with us!!"
+      end
+    end
   end
-end
-end
 end
